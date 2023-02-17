21bdebeeb1e5546f6e1e35b37558d61b3b88bd19
ryota-murakami <dojce1048@gmail.com>
2023-02-17 17:34:10 +0900

    update to no-unused-vars: ["error", {"args": "after-used"}]

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = {
   root: true,
   rules: {
     '@typescript-eslint/consistent-type-imports': 'warn',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'after-used' }],
     '@typescript-eslint/prefer-as-const': 'warn',
     eqeqeq: ['error', 'always'],
     'import/default': 'error',
