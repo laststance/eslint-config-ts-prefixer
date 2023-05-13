8698f6ce3931650ffa7f0b7f017b0ca488444dfd
ryota-murakami <dojce1048@gmail.com>
2023-05-14 08:53:27 +0900

    add 'import/no-cycle': 'error'

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports = {
     'import/default': 'error',
     'import/export': 'error',
     'import/named': 'error',
+    'import/no-cycle': 'error',
     'import/no-duplicates': 'error',
     'import/no-named-as-default': 'error',
     'import/no-unresolved': 'error',
