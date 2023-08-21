ea9c98e9f711e8ce3083e70805508f4615fbcc89
ryota-murakami <dojce1048@gmail.com>
2023-08-21 18:09:46 +0900

    remove @typescript-eslint/no-unnecessary-condition

--- index.cjs
+++ index.cjs
@@ -30,12 +30,6 @@ module.exports = {
       },
     ],
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
-    '@typescript-eslint/no-unnecessary-condition': [
-      'error',
-      {
-        allowConstantLoopConditions: true,
-      },
-    ],
     '@typescript-eslint/no-unused-expressions': 'error',
 
     // Allow foo(unuseArg, useValue) & bar(_, useValue) & { foo, ...coords } = data; https://typescript-eslint.io/rules/no-unused-vars/
